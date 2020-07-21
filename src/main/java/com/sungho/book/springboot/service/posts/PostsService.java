1ee5ea1d53f25c6a4b1b1fd37be8b6725f0c8b0e
KimSungHo <starho@sharemind.kr>
2020-07-21 17:29:35 +0900

    Spring Boot,JPA 를 활용한 웹서비스

--- src/main/java/com/sungho/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sungho/book/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.sungho.book.springboot.service.posts;
 
 import com.sungho.book.springboot.domain.posts.Posts;
 import com.sungho.book.springboot.domain.posts.PostsRepository;
+import com.sungho.book.springboot.web.dto.PostsListRseponseDto;
 import com.sungho.book.springboot.web.dto.PostsResponseDto;
 import com.sungho.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sungho.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //롬복의 어노테이션 final이 선언된 모든 필드를 자동 생성 @Autowired를 사용 안해도된다
 @Service
@@ -33,5 +37,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListRseponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListRseponseDto::new).collect(Collectors.toList());
     }
 }

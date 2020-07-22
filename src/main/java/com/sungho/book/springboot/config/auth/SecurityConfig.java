a59c51b9b8e3f63900e2600597139b1e19653a4d
KimSungHo <starho@sharemind.kr>
2020-07-22 14:53:09 +0900

    Spring Boot,JPA 를 활용한 웹서비스 구글로그인 추가

---
+++ src/main/java/com/sungho/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.sungho.book.springboot.config.auth;
+
+import com.sungho.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+    }
+        //1. csrf().disable().headers().frameOptions().disable(): h2-console화면을 사용하기 위해 해당 옵션들을 disable 한다.
+
+        //2. authorizeRequests: Url별 권한 권리를 설정하는 옵션의 시작점. 이것을 선언해야만 antMatchers 옵션을 사용가능
+
+        /*3. andMatchers: 권한 관리 대상을 지정하는 옵션. URL,HTTP 메소드별로 관리가 가능.
+            "/"등 시전된 URL들은 permitAll()옵션을 통해 전체 열람 권한을 줌,
+            "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 함.*/
+
+        /*4. anyRequset: 설정된 값들 이외 나머지 URL들을 나타낸다.
+            여기선 authenticathe()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용, 인증된 사용자는 로그인한 사용자. */
+
+        //5.logout().logoutSussessUrl("/") : 로그아웃 기능에 대한 여러설정의 진입점, 로그아웃 성공시 /주소로 이동
+
+        //6. oauth2Login: OAuth2 로그인 기능에 대한 여러 설정의 진입점
+
+        //7. userInfoEndpoint: OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들은 담당
+
+        /*8. userService: 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+            리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기눙을 명시할 수 있다.*/
+
+}

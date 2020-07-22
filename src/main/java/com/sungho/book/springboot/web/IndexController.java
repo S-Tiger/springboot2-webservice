662a95e3f05bbcfa4ab3d8eb8f4a75c16701cdd1
KimSungHo <starho@sharemind.kr>
2020-07-22 15:53:49 +0900

    Spring Boot,JPA 를 활용한 웹서비스 로그인체크 어노테이션생성

--- src/main/java/com/sungho/book/springboot/web/IndexController.java
+++ src/main/java/com/sungho/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sungho.book.springboot.web;
 
+import com.sungho.book.springboot.config.auth.LoginUser;
 import com.sungho.book.springboot.config.auth.dto.SessionUser;
 import com.sungho.book.springboot.service.posts.PostsService;
 import com.sungho.book.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpsession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpsession.getAttribute("user");
+
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
